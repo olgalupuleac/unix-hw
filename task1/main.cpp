cbfea0d3f95b062e46af332d811824a4d1af5c1a
Olga Lupuleac <olga_lupuleac@mail.ru>
2017-04-30 14:40:34 +0300

    task1

---
+++ task1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	ifstream ifs("MBR");
+	char partition[16];
+	ifs.seekg(446);
+	int cnt = 0;
+	for (int i = 0; i < 4; i++) {
+		ifs.read(partition, sizeof partition);
+		for(int j = 0; j < 16; j++)
+			if (partition[j] != 0) {
+				cnt++;
+				break;
+			}
+	}
+	cout << cnt << endl;
+	return 0;
+}
