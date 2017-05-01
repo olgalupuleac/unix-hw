b4881143f2b92df650a4a0960d7b7bff55ab90bf
Olga Lupuleac <olga_lupuleac@mail.ru>
2017-05-01 19:48:59 +0300

    changed main get commands

--- task3/main.cpp
+++ task3/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include <cstdlib>
 #include <vector>
 #include <string>
@@ -13,10 +14,11 @@ int main(){
     is >> freq;
     string commands;
     while(! is.eof()){
-        char cmd[1000];
-        is.getline(cmd, 1000);
-        commands += ((string(cmd)) + "\n");
+		char c = is.get();
+        if(! is.eof())
+            commands += c;
     }
+    cout << commands << endl;
     while(true){
         system(commands.c_str());
         this_thread::sleep_for(chrono::minutes(freq));
